13140d2d4a7f2cb59d8ab4499eb5ac6127694d05
zaknoun <issamzantout18@gmail.com>
2025-10-13 19:42:13 -0700

    fixed dijkstra door issue

--- Backend/front/vite.config.js
+++ Backend/front/vite.config.js
@@ -29,6 +29,8 @@ export default defineConfig({
         mainDungeon: resolve(__dirname, "./mainDungeon.html"),
         prim: resolve(__dirname, "./Prim.html"),
         dashboard: resolve(__dirname, "./dashboard.html"),
+        dashboard: resolve(__dirname, "./dashboard.html"),
+        dijkstra: resolve(__dirname, "./Dijkstra.html"),
         // Add more HTML files as needed
       },
     },
